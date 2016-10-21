9568cc73a88c2c7d4e8d08d6b7635656ffe8882b
Aldo Barba <aldo.barba@inmediatum.com>
2016-10-21 11:40:23 -0500

    ignore config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-exports.values = {
-    debug : true,
-	port : 8080,
-	log : {
-		console_level : 'silly', 		// silly, debug, verbose, info, warn, error
-		file_level : 'info',		// info, warn, error
-		file : 'errors.log'
-	}
-}
